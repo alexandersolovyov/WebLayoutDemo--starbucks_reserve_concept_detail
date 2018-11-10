9c971a74ac90f0ea7a32a1aa40891656af6b409b
Aleksandr Solovyov <solovyov-alexander@yandex.ru>
2018-11-10 22:52:47 +0300

    Add dropdown menu for menu button.

--- js/main.js
+++ js/main.js
@@ -26,18 +26,33 @@ if (typeof MAIN !== 'object') {
    * Private functions
    */
 
+  /*
+   * Toggles menu.
+   * Gets menu element.
+   */
+  var menuToggle = function(menu) {
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    }else {
+      menu.style.display = "block";
+    }
+  }
+
   /*--------------------------------------------------------------
    * Interface functions
    */
   if (typeof MAIN.init !== 'function') {
     MAIN.init = function() {
+      // Get dropdown menu
+      var menu = document.getElementById('menu');
       // Get all buttons and links except menu and logo
       // and add actions for them
       var buttons = document.getElementsByTagName('button');
-      console.log(buttons.length);
       for (var i=0; i<buttons.length; ++i) {
         if (buttons[i].className.indexOf('Menu') !== -1) {
-          console.log(buttons[i]);
+          buttons[i].onclick = function() {
+            menuToggle(menu);
+          };
         }else {
           buttons[i].onclick = function() {
             window.alert(DummyAlertText);
